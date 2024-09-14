6b8f616cd8d2e85de2ca171f773c2c271da21576
Mayank Singh <mayank757511@gmail.com>
2024-09-14 16:57:01 +0530

    Functionalities added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,23 @@
+import StreamVideoProvider from '@/providers/StreamClientProvider'
+import { Metadata } from 'next';
 import React, { ReactNode } from 'react'
 
-const RootLayout = ({children}:{children : ReactNode}) => {
+export const metadata: Metadata = {
+  title: "Yoom",
+  description: "Video conferencing app",
+  icons:{
+    icon:'/icons/logo.svg'
+  }
+};
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <main>
-     {children}
+      <StreamVideoProvider>
+        {children}
+      </StreamVideoProvider>
     </main>
   )
 }
 
-export default  RootLayout
+export default RootLayout
